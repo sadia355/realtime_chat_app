14003fc9fcff323514d98f463f45815967bfbe06
ahmedsadia2022 <sadia.orpita@northsouth.edu>
2026-09-17 22:18:17 +0600

    1943

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes)
 
-app.listen(5173, () => {
-    console.log("server is running on port 5173");
+app.listen(PORT, () => {
+    console.log("server is running on PORT "+PORT);
 });
