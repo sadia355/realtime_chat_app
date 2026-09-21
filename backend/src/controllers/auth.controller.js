9c00f23d64ec579538fc93fb0148af514b8fc7f0
sadia355 <sadia355@gmail.com>
2026-09-21 20:54:20 +0600

    3350

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,12 +1,37 @@
-    
-export const signup = (req, res) => {
-    res.send("signup route");
-}
+import User from "../models/user.model.js"; 
+import bcrypt from "bcrypt.js"   
+export const signup = async(req, res) => {
+   const{fullName, email, password} = re.body
+    try {
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be at least 6 characters long"});
+        }
+        
+        const user = await User.findOne({email});
+        if(user) return res.status(400).json({message: "Email already exists"});
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+        const newUser = new User({ 
+            fullName,
+            email,
+            password:hashPassword
+        })    
+        
+        if(newUser){
+
+        }
+        else{
+            res.status(400).json({message: "Invalid user data"});
+        }
+    } catch (error) {
+       
+    }
+};
 
 export const login = (req, res) => {
     res.send("login route");
-}
+};
 
 export const logout = (req, res) => {
     res.send("logout route");
-}
+};
