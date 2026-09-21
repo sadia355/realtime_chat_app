c474869978fb020d8ab905f0aa0f1aef569a70b4
sadia355 <sadia355@gmail.com>
2026-09-21 20:20:33 +0600

    connected now

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js";
+
 
 dotenv.config();
 const app = express();
@@ -10,4 +12,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log("server is running on PORT "+PORT);
+    connectDB();
 });
